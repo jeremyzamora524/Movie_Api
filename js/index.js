3e6f41e2a5d00c55dda683cfe6576af1834cc5ba
jeremyzamora524 <jeremyzamora524@gmail.com>
2022-05-04 23:38:09 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -28,18 +28,18 @@ app.use(bodyParser.json()); //parsing headerbody
 app.use(bodyParser.urlencoded({ extended: true})); //parsing headerbody
 
 const cors = require('cors');
-//let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
 
-//app.use(cors({
-  //origin: (origin, callback) => {
-    //if(!origin) return callback(null, true);
-    //if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      //let message = 'The CORS policy for this application doesnt allow access from origin ' + origin;
-      //return callback(new Error(message ), false);
-    //}
-    //return callback(null, true);
-  //}
-//}));
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesnt allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 let auth = require('./auth')(app); // imports our auth.js file
 const passport = require('passport'); // imports passport module
